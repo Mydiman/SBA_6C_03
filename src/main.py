5bdd1d8acb491f210499f6d12931519f48c629ca
Mydiman <140135195+Mydiman@users.noreply.github.com>
2023-11-29 22:03:40 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -15,12 +15,12 @@ if __name__ == '__main__':
 
 from time import sleep
 from sys import exc_info
-from traceback import print_tb
 from .ClearScreen import clearscreen
 from .Colour import Colour
 from .LogIn import log_in
 from .UserVersion import user_version
 from .AdminVersion import admin_version
+from .Resetsystem import reset_sys
 
 
 
@@ -29,34 +29,48 @@ def main() -> None:
 	print(Colour.Reset)
 	#Don't forgot check data folder exist!!!
 	login_version = 3
+	byebye = 0
 	error = 0
 	while True:
-		#try:
+		try:
 			if error == 0:
+				clearscreen()
 				login_version = log_in()
 			else:
 				clearscreen()
 				print(Colour.Red + "THERE IS AN EXCEPTION { " + exception_type.__name__ + " } DETECTED" + Colour.Reset)
-				print(print_tb(exception_line))
-				if login_version == 1:
+				if exception_type.__name__ == "FileNotFoundError":
+					reset_sys()
+					login_version = 3
+					print(Colour.Red + "SYSTEM RESETED" + Colour.Reset)
+					print("Username of admin: admin")
+					print("Password of admin: pass")
+					print("Username of admin: user")
+					print("Password of admin: bhjs")
+					input(Colour.Yellow + "ENTER TO CONTINUE" + Colour.Reset)
+				elif exception_type.__name__ == "ImportError":
+					print(Colour.Red + "THERE IS AN EXCEPTION { " + exception_type.__name__ + " } DETECTED" + Colour.Reset)
+					print(Colour.Red + "PLEASE NEVER EDIT .py FILE" + Colour.Reset)
+				elif login_version == 1:
 					input(Colour.Yellow + "ENTER TO GO BACK TO ADMIN MENU" + Colour.Reset)
 				elif login_version == 2:
 					input(Colour.Yellow + "ENTER TO GO BACK TO USER MENU" + Colour.Reset)
 				else:
 					input(Colour.Yellow + "ENTER TO CONTINUE" + Colour.Reset)
-			clearscreen()
 			error = 0
 			if login_version == 1:
-				if admin_version() == 1:
-					list_quit = ["\\", "|", "/", "-"]
-					for i in range(0, 3):
-						for j in list_quit:
-							print("\rShuting down in {} seconds {}".format(3 - i, j), end = "")
-							sleep(0.25)
-					exit()
+				clearscreen()
+				byebye = admin_version()
 			elif login_version == 2:
+				clearscreen()
 				user_version()
-		#except:
-		#	exception_type = exc_info() [0]
-		#	exception_line = exc_info() [2]
-		#	error = 1
+			if byebye == 1:
+				list_quit = ["\\", "|", "/", "-"]
+				for i in range(0, 3):
+					for j in list_quit:
+						print("\rShuting down in {} seconds {}".format(3 - i, j), end = "")
+						sleep(0.25)
+				exit()
+		except:
+			exception_type = exc_info() [0]
+			error = 1
